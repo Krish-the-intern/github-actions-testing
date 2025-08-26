60a415c4bb4bd6f5a352761b75701d235b9606fd
Krishang <krishang@mtradeasia.com>
2025-08-26 16:30:34 +0800

    removed hello.js and hello.test.js

--- hello.js
+++
@@ -1,8 +0,0 @@
-function hello(name) {
-  return `Hello, ${name}!`;
-}
-
-console.log(hello("GitHub Actions"));
-module.exports = hello;
-
-// PR test change
--- hello.test.js
+++
@@ -1,6 +0,0 @@
-const hello = require('./hello');
-
-test('greets correctly', () => {
-  expect(hello('GitHub Actions')).toBe('Hello, GitHub Actions!');
-});
-// PR test change
