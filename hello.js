4517ab97898f87f6127138c9d79579af30af0837
Krishang <krishang@mtradeasia.com>
2025-08-26 11:33:06 +0800

    Add project files with tests and CI workflow

---
+++ hello.js
@@ -0,0 +1,6 @@
+function hello(name) {
+  return `Hello, ${name}!`;
+}
+
+console.log(hello("GitHub Actions"));
+module.exports = hello;
---
+++ hello.test.js
@@ -0,0 +1,5 @@
+const hello = require('./hello');
+
+test('greets correctly', () => {
+  expect(hello('GitHub Actions')).toBe('Hello, GitHub Actions!');
+});
